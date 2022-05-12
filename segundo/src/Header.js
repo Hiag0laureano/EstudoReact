489f4e585b36842fd3ae7ef4b311a36f7970e266
HIAGO LAUREANO DE FREITAS SANTOS <hiago.lfsantos@senacsp.edu.br>
2022-05-12 11:56:06 -0300

    Criação de pagina usando o react

---
+++ segundo/src/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className="header">
+      <img
+        alt="logo do curso"
+        src="https://www.geekforce.com.br/wp-content/uploads/2021/02/Geek-Force-Robots-School.png"
+      />
+      <h1>Programação de Cursos</h1>
+      <h3>Março - Abril - Maio</h3>
+    </div>
+  );
+}
+export default Header;
---
+++ segundo/src/Main.js
@@ -0,0 +1,24 @@
+function Main() {
+  return (
+    <div>
+      <h4>Bem-Estar</h4>
+      <ul>
+        <li>Reiki - Master</li>
+        <li>Reiki - Nível 1 e 2</li>
+        <li>Reiki - Nível 3</li>
+      </ul>
+      <h4>Comunicação e Artes</h4>
+      <ul>
+        <li>Criação de Conteúdo para Mídias Digitais</li>
+        <li>Cração de Posts paa Redes Socias</li>
+        <li>Formação Básica em Fotografia</li>
+        <li>Fotografia: Social e Eventos Corporativos</li>
+        <li>Gestão de Perfil para Redes Sociais</li>
+        <li>Iluminação Fotográfica com Flash</li>
+        <li>Lightroom Basico</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Main;

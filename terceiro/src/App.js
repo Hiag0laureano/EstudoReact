8869073103f7f73d38bf7cb1e22b3c65ab8a83d7
HIAGO LAUREANO DE FREITAS SANTOS <hiago.lfsantos@senacsp.edu.br>
2022-05-13 09:45:11 -0300

    Definindo Styles dentro do app.js

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,15 +1,14 @@
 function App() {
-  let valor1 = Math.round(Math.random() * 200);
-  let valor2 = Math.round(Math.random() * 200);
+  let styles = {
+    backgroundColor: "#300",
+    color: "#fff",
+    display: "flex",
+    flex: 1,
+    height: "100vh",
+  };
   return (
-    <div>
-      <p>A soma entre 50 e 10 é: {50 + 10}</p>
-      <p>
-        A soma entre {valor1} e {valor2} é: {valor1 + valor2}
-      </p>
-      <p>
-        O resultado a cima é: {(valor1 + valor2) % 2 === 0 ? "Par" : "Impar"}
-      </p>
+    <div style={styles}>
+      <p>Você é programador cara!!!</p>
     </div>
   );
 }

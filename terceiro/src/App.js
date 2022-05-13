fc1687a69337716790d517c8306c76d1e834c071
HIAGO LAUREANO DE FREITAS SANTOS <hiago.lfsantos@senacsp.edu.br>
2022-05-13 09:18:23 -0300

    executando javascript dentro da area de retorno

---
+++ terceiro/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  let valor1 = Math.round(Math.random() * 200);
+  let valor2 = Math.round(Math.random() * 200);
+  return (
+    <div>
+      <p>A soma entre 50 e 10 é: {50 + 10}</p>
+      <p>
+        A soma entre {valor1} e {valor2} é: {valor1 + valor2}
+      </p>
+      <p>
+        O resultado a cima é: {(valor1 + valor2) % 2 === 0 ? "Par" : "Impar"}
+      </p>
+    </div>
+  );
+}
+
+export default App;

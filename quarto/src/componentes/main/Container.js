667088e47c9fe707da85b30ac7df62ae99681583
HIAGO LAUREANO DE FREITAS SANTOS <hiago.lfsantos@senacsp.edu.br>
2022-05-17 11:58:36 -0300

    Continuação da configuração do recat com api do sevidor

--- quarto/src/componentes/main/Container.js
+++ quarto/src/componentes/main/Container.js
@@ -1,11 +1,35 @@
 import Conteudo from "./Conteudo";
 import Mensagens from "./Mensagens";
+import { useState, useEffect } from "react";
 
 export default function Container() {
+  // Vamos iniciar o projeto lendo a url com os dados da api
+  // neste caso temos os produtos para ler montar a tela do conteudo
+  // Vamos usar o comando useState para iniciar o estado de dados
+  // dos produtos. Depois usaremos o comando useEffect para carregar os produtos que vem da api.
+  // O comando useEffect é um comando de laço, portanto é necessario fazer ele parar o laço quando termina de carregar os dados.
+  // Isso é feito usando um colchetes ao final da instruções. Para obter os dados iremos usar o comando fetch dentro useEffect.
+
+  const [produtos, setProdutos] = useState([
+    {
+      id: "",
+      nome: "",
+      categoria: "",
+      preco: "",
+      msg: [],
+    },
+  ]);
+  useEffect(() => {
+    fetch("http://10.26.44.24:5500/")
+      .then((response) => response.json())
+      .then((dados) => {
+        setProdutos(dados.output);
+      });
+  }, []);
   return (
     <div className="container">
-      <Mensagens />
-      <Conteudo />
+      <Mensagens dados={produtos} />
+      <Conteudo dados={produtos} />
     </div>
   );
 }
--- quarto/src/componentes/main/Conteudo.js
+++ quarto/src/componentes/main/Conteudo.js
@@ -1,8 +1,16 @@
-export default function Conteudo() {
+export default function Conteudo(props) {
   return (
     <div className="conteudo">
       <h2>Produtos</h2>
-      {/* <img src="" alt="" /> */}
+      <div className="itens">
+        {props.dados.map((itens, index) => (
+          <div key={index}>
+            <h3>{itens.nome}</h3>
+            <p>{itens.categoria}</p>
+            <p className="preco">R${itens.preco}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
